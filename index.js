078077b44739fe2344d97d8d96fef7099edefaa7
Briggs Elsperger <I3uckwheat@gmail.com>
2017-11-24 17:45:43 -0600

    adds books to library array

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("test");
+let myLibrary = [];
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  function info(){
+    return `${title} by ${author}, ${pages} pages long, ${read ? "read" : "not yet read"}.`
+  }
+}
+
+function addBookToLibrary() {
+  const bookName = prompt("Input Book Name");
+  const author = prompt("Input Author");
+  const pages = prompt("input pages");
+  const readStatus = (/y/.test(prompt("Input Y/N for read status")));
+
+  myLibrary.push(new Book(bookName, author, pages, readStatus));
+}
